1ac8928936934303c80aaf769cc81ed423ce49b1
Wangcheng Yan <wyan3@vols.utk.edu>
2019-01-30 11:53:48 -0500

    edit zBMI

--- R/zBMI.R
+++ R/zBMI.R
@@ -1,25 +1,21 @@
-Score_age <- function(data){
-  data$DEM2AGE_YEAR_NORM <- 1919 + data$DEM2AGE_YEAR
-  data$DEM2AGE_YEAR_NORM[data$DEM2AGE_YEAR==99] <- NA
-  data$DEM2AGE_MONTH_NORM <- data$DEM2AGE_MONTH
-  data$DEM2AGE_MONTH_NORM[data$DEM2AGE_MONTH==13] <- NA
-  data$DEM2AGE_DATE_NROM <- data$DEM2AGE_DATE
-  data$DEM2AGE_DATE_NROM[data$DEM2AGE_DATE==32] <- NA
-  data$DOB <- paste(paste(data$DEM2AGE_MONTH_NORM,data$DEM2AGE_DATE_NROM,sep="/"),data$DEM2AGE_YEAR_NORM,sep="/")
-  data$DOB <- as.Date(data$DOB,"%m/%d/%Y")
-  data$ageinmonth <- rep(NA,nrow(data))
-  for(i in 1:nrow(data)){
-    EOD <- as.Date(strsplit(as.character(data$StartDate[i])," ")[[1]][1],"%m/%d/%y")
-    if(!is.na(data$DOB[i]) & !is.na(EOD)){
-      data$ageinmonth[i] <- age_calc(dob = data$DOB[i], enddate = EOD, units = "months")
+Score_zBMI <- function(data,age_month=FALSE){
+  if(!age_month){
+    data$DEM2AGE_YEAR_NORM <- 1919 + data$DEM2AGE_YEAR
+    data$DEM2AGE_YEAR_NORM[data$DEM2AGE_YEAR==99] <- NA
+    data$DEM2AGE_MONTH_NORM <- data$DEM2AGE_MONTH
+    data$DEM2AGE_MONTH_NORM[data$DEM2AGE_MONTH==13] <- NA
+    data$DEM2AGE_DATE_NROM <- data$DEM2AGE_DATE
+    data$DEM2AGE_DATE_NROM[data$DEM2AGE_DATE==32] <- NA
+    data$DOB <- paste(paste(data$DEM2AGE_MONTH_NORM,data$DEM2AGE_DATE_NROM,sep="/"),data$DEM2AGE_YEAR_NORM,sep="/")
+    data$DOB <- as.Date(data$DOB,"%m/%d/%Y")
+    data$ageinmonth <- rep(NA,nrow(data))
+    for(i in 1:nrow(data)){
+      EOD <- as.Date(strsplit(as.character(data$StartDate[i])," ")[[1]][1],"%m/%d/%y")
+      if(!is.na(data$DOB[i]) & !is.na(EOD)){
+        data$ageinmonth[i] <- age_calc(dob = data$DOB[i], enddate = EOD, units = "months")
+      }
     }
   }
-  return(data$ageinmonth)
-}
-
-
-Score_zBMI <- function(data,age_month=FALSE){
-  if(!age_month){data$ageinmonth <- Score_age(data)}
   data$ageinmonth_round <- floor(data$ageinmonth) + 0.5
   
   data$zBMI <- rep(NA,nrow(data))
