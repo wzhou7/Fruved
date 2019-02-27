d75e7195f1e88a8574dea174620d81ae92a41446
Wangcheng Yan <wyan3@vols.utk.edu>
2019-02-27 15:22:50 -0500

    Priorities scoring

---
+++ R/Priorities.R
@@ -0,0 +1,37 @@
+Score_Priorities <- function(data){
+  df <- data[,paste0("PRIORITIES",1:31)]
+  dg <- as.data.frame(matrix(NA,nrow=2,ncol=ncol(df)))
+  for(j in 1:ncol(df)){
+    df[,j][df[,j]==6 | df[,j]==7] <- NA
+    dg[1,j] <- sum(!is.na(df[,j]))
+    dg[2,j] <- mean(df[,j],na.rm=TRUE)
+  }
+  if(dg[1,31]<0.5*dg[1,1]){
+    dh <- dg[,-31]
+  }
+  if(dg[1,31]>=0.5*dg[1,1]){
+    dh <- dg[,-31]
+  }
+  all_index <- order(dh[2,],decreasing = TRUE)
+  index <- all_index[1:5]
+  scores <- rep(NA,16)
+  for(i in 1:5){
+    if(dh[1,index[i]]>0){
+      scores[3*(i-1)+1] <- map_priorities[,2][map_priorities[,1]==paste0("PRIORTIES_",index[i])]
+      scores[3*(i-1)+2] <- dh[1,index[i]]
+      scores[3*(i-1)+3] <- dh[2,index[i]]
+    }
+  }
+  if(!is.na(dh[2,all_index[5]]) & !is.na(dh[2,all_index[6]])){
+    if(dh[2, all_index[5]] == dh[2, all_index[6]]){
+      scores[16] <- map_priorities[,2][map_priorities[,1]==paste0("PRIORTIES_",all_index[6])]
+      for(j in 7:length(all_index)){
+        if(!is.na(dh[2,all_index[j]]) & (dh[2,all_index[j]]==dh[2,all_index[5]])){
+          scores[16] <- paste(scores[16],
+                              map_priorities[,2][map_priorities[,1]==paste0("PRIORTIES_",all_index[j])],sep=" AND ")
+        }
+      }
+    }
+  }
+  return(scores)
+}
