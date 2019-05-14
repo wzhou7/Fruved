c708411c1ef2193c16e21f8d9c2e7f43d725d18d
Wangcheng Yan <wyan3@vols.utk.edu>
2019-05-14 00:15:02 -0400

    add R and Rd files

--- R/Personality.R
+++ R/Personality.R
@@ -5,16 +5,23 @@ Score_Personality <- function(data){
     df[,j] <- as.numeric(as.character(df[,j]))
   }
   dg <- df
-  for(j in c(2,4,6,8,10)){
-    dg[,j] <- 8 - df[,j]
+  if(min(dg[,1],na.rm=TRUE)<1 | max(dg[,1],na.rm=TRUE)>7){
+    return("Warning: Check the coded values of Choose not to answer")
   }
-  
-  Extraversion <- (dg[,1] + dg[,6])/2
-  Agreeableness <- (dg[,2] + dg[,7])/2
-  Conscientiousness <- (dg[,3] + dg[,8])/2
-  Emotional_Stability <- (dg[,4] + dg[,9])/2
-  Openness_to_Experiences <- (dg[,5] + dg[,10])/2
-  p.score <- cbind(Extraversion,Agreeableness,Conscientiousness,Emotional_Stability,Openness_to_Experiences)
-  
-  return(as.data.frame(p.score))
+  else{
+    for(j in c(2,4,6,8,10)){
+      dg[,j] <- 8 - df[,j]
+    }
+    
+    Extraversion <- (dg[,1] + dg[,6])/2
+    Agreeableness <- (dg[,2] + dg[,7])/2
+    Conscientiousness <- (dg[,3] + dg[,8])/2
+    Emotional_Stability <- (dg[,4] + dg[,9])/2
+    Openness_to_Experiences <- (dg[,5] + dg[,10])/2
+    p.score <- cbind(Extraversion,Agreeableness,Conscientiousness,Emotional_Stability,Openness_to_Experiences)
+    names(p.score) <- c("P_extraversion","P_agreeableness","P_conscientiousness",
+                        "P_emotionalstability","P_openess")
+    
+    return(as.data.frame(p.score))
+  }
 }
