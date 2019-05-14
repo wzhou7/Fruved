5253e07759c9551942b17407cdb88329094a7fb4
Wangcheng Yan <wyan3@vols.utk.edu>
2019-05-14 11:19:52 -0400

    edited ED

--- R/ED.R
+++ R/ED.R
@@ -2,20 +2,22 @@ Score_ED <- function(data){
   seq <- paste("ED_",9:34,sep="")
   for(j in 1:25){
     comp <- paste("Comp",j,sep="")
-    data[,comp] <- rep(0,nrow(data))
+    data[,comp] <- rep(NA,nrow(data))
     data[,comp][data[,seq[j]]==1] <- 3
     data[,comp][data[,seq[j]]==2] <- 2
     data[,comp][data[,seq[j]]==3] <- 1
-    data[,comp][data[,seq[j]]==7] <- NA
-    data[,comp][is.na(data[,seq[j]])] <- NA
+    #data[,comp][data[,seq[j]]==7] <- NA
+    #data[,comp][is.na(data[,seq[j]])] <- NA
+    data[,comp][data[,seq[j]] %in% c(4,5,6)] <- 0
   }
   
-  data$Comp26 <- rep(0,nrow(data))
+  data$Comp26 <- rep(NA,nrow(data))
   data$Comp26[data[,seq[26]]==6] <- 3
   data$Comp26[data[,seq[26]]==5] <- 2
   data$Comp26[data[,seq[26]]==4] <- 1
-  data$Comp26[data[,seq[26]]==7] <- NA
-  data$Comp26[is.na(data[,seq[26]])] <- NA
+  #data$Comp26[data[,seq[26]]==7] <- NA
+  #data$Comp26[is.na(data[,seq[26]])] <- NA
+  data$Comp26[data[,seq[26]] %in% c(1,2,3)] <- 0
   
   ED_SCORE <- data$Comp1 + data$Comp2 + data$Comp3 + data$Comp4 + data$Comp5 + data$Comp6 +
     data$Comp7 + data$Comp8 + data$Comp9 + data$Comp10 + data$Comp11 + data$Comp12 + data$Comp13 +
