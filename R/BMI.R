a8c33bd1cbb5af47ad7f25a4bf2dd885571afa0d
Wenjun Zhou <wzhou7@gmail.com>
2024-01-20 21:30:31 -0500

    implement BMI

--- R/BMI.R
+++ R/BMI.R
@@ -1,10 +1,98 @@
-Score_BMI <- function(data, ht="Height", wt="Weight"){
-  names(data)[names(data)==ht] <- "Height"
-  names(data)[names(data)==wt] <- "Weight"
-  BMI <- data$Weight/((data$Height/100)^2)
-  if(median(BMI,na.rm=TRUE) < 10 | median(BMI,na.rm=TRUE) > 65){
-    warning("BMI range is abnormal. Make sure the unit of height is centimeter (cm) and the unit of weight is kilogram (kg)")
-  }
-  return(BMI)
+ConvertWt <- function(data, wt = "WeightLB", wt_unit = "lb"){
+    # convert weight into KG
+    wt_unit <- tolower(wt_unit)
+    if(wt_unit=="lb"){
+        WeightKG <- data[,wt] * 0.45359237 # pound to kilogram
+        return(WeightKG)
+    } else {
+        stop(paste("The unit",wt_unit,"is not implemented!"))
+    }
 }
 
+ConvertHt <- function(data, ht = "HeightIN", ht_unit = "in"){
+    # convert height into M
+    ht_unit <- tolower(ht_unit)
+    if(ht_unit=="in"){
+        HeightM <- data[,ht] * 0.0254 # inch to meter
+        return(HeightM)
+    } else {
+        stop(paste("The unit",ht_unit,"is not implemented!"))
+    }
+}
+
+flag_plausible <- function(x, type="weight", min_plausible=2, max_plausible=800){
+    y <- rep("", length(x))
+    y[is.na(x)] <- paste("missing", type)
+    y[(!is.na(x))&(x<min_plausible)] <- paste("implausible",type,"(too low)")
+    y[(!is.na(x))&(x>max_plausible)] <- paste("implausible",type,"(too high)")
+    return(y)
+}
+
+flag_outlier <- function(x, type, y){
+    num_obs <- sum(!is.na(x))
+    if(num_obs >= 30){
+        xMedian <- median(x, na.rm=TRUE)
+        xIQR <- IQR(x, na.rm=TRUE)
+        
+        extreme_low_cut <- xMedian - 3 * xIQR
+        low_cut <- xMedian - 1.5 * xIQR
+        y[(y=="")&(x<extreme_low_cut)] <- paste(type, "outlier (extrmely low)")
+        y[(y=="")&(x<low_cut)] <- paste(type, "outlier (low)")
+        
+        extreme_high_cut <- xMedian + 3 * xIQR
+        high_cut <- xMedian + 1.5 * xIQR
+        y[(y=="")&(x>extreme_high_cut)] <- paste(type, "outlier (extrmely high)")
+        y[(y=="")&(x>high_cut)] <- paste(type, "outlier (high)")
+    }
+    return(y)
+}
+
+Score_BMI <- function(data, data, wt="WeightLB", ht = "HeightIN", wt_unit = "lb", ht_unit = "in"){
+    
+    # if these variables already exist, rename them
+    varnames <- c("WeightKG","WeightFlag","HeightM","HeightFlag","BMI","BMIFlag","BMI_Category")
+    for(v in varnames){
+        v_old <- paste0(v,"_old")
+        data[,v_old] <- data[,v]
+    }
+    
+    # convert weight into KG
+    wt_unit <- tolower(wt_unit)
+    if(wt_unit!="kg"){
+        data$WeightKG <- ConvertWt(data[,wt])
+    } else {
+        data$WeightKG <- data[,wt]
+    }
+    
+    # flag implausible values and outliers
+    data$WeightFlag <- flag_plausible(data$WeightKG, "weight", min_plausible=2, max_plausible=800)
+    data$WeightFlag <- flag_outlier(data$WeightKG, "weight", data$WeightFlag)
+    
+    # convert height into M
+    ht_unit <- tolower(ht_unit)
+    if(ht_unit!="m"){
+        data$HeightM <- ConvertHt(data[,ht])
+    } else {
+        data$HeightM <- data[,ht]
+    }
+    
+    # flag implausible values and outliers
+    data$HeightFlag <- flag_plausible(data$HeightM, "height", min_plausible=0.5, max_plausible=3.0)
+    data$HeightFlag <- flag_outlier(data$HeightM, "height", data$HeightFlag)
+    
+    # calculate BMI
+    data$BMI <- data$WeightKG/data$HeightM/data$HeightM
+    
+    # flag implausible values and outliers
+    data$BMIFlag <- flag_plausible(data$BMI, "BMI", min_plausible=5, max_plausible=200)
+    data$BMIFlag <- flag_outlier(data$BMI, "BMI", data$BMIFlag)
+    
+    # categorize BMI
+    data$BMI_Category <- NA
+    data$BMI_Category[(!is.na(data$BMI))&(data$BMI<18.5)] <- "Underweight"
+    data$BMI_Category[(!is.na(data$BMI))&(data$BMI>=18.5)&(data$BMI<25)] <- "Healthy Weight"
+    data$BMI_Category[(!is.na(data$BMI))&(data$BMI>=25)&(data$BMI<30)] <- "Overweight"
+    data$BMI_Category[(!is.na(data$BMI))&(data$BMI>=30)] <- "Obesity"
+    
+    return(data)
+}
--- docs/BMI.md
+++ docs/BMI.md
@@ -1,8 +1,10 @@
 # Body Mass Index (BMI)
 
 The BMI is often used as a cost effective screening tool for a variety of health metrics.
+For children and teens, their BMIs should incorporate the growth chart. 
+See [Growth Chart Standardized BMI](zBMI.md) (zBMI).
 
-For adults, It is calculated as a person's weight in kilograms 
+For adults, the BMI is calculated as a person's weight in kilograms 
 divided by the square of height in meters. The calculation formula is: 
 
 $$
@@ -11,28 +13,25 @@ $$
 \end{aligned}
 $$
 
-According to CDC, ``For adults 20 years old and older, BMI is interpreted using standard weight status categories. 
-These categories are the same for men and women of all body types and ages.''
+According to CDC, "For adults 20 years old and older, BMI is interpreted using standard weight status categories. 
+These categories are the same for men and women of all body types and ages.""
 The categories are:
 
 | BMI            | Weight Status  |
 | -------------- | -------------- |
 | Below 18.5     | Underweight    |
-| 18.5 ¨C 24.9    | Healthy Weight |
-| 25.0 ¨C 29.9    | Overweight     |
+| 18.5 - 24.9    | Healthy Weight |
+| 25.0 - 29.9    | Overweight     |
 | 30.0 and Above | Obesity        |
 
 More information can be found on the [CDC website](https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html) and the [Wikipedia](https://en.wikipedia.org/wiki/Body_mass_index).
-
-For children and teens, their BMIs should incorporate the growth chart. 
-See [Growth Chart Standardized BMI](zBMI.md) (zBMI).
 
 ## How to Use
 
 After importing your data as a data frame, use the following code to obtain the BMI score:
 
 ```
-data <- Score_BMI(data, wt="WtLB", ht = "HtIN", wt_unit = "lb", ht_unit = "in")
+data <- Score_BMI(data, wt="WeightLB", ht = "HeightIN", wt_unit = "lb", ht_unit = "in")
 ```
 
 Be careful of specifying the correct measurement units, so the numbers will be converted correctly.
@@ -47,6 +46,6 @@ If you would like to convert the units and save the converted values in the data
 follow this example:
 
 ```
-data$WtKG <- ConvertWt(data, wt = "WtLB", wt_unit = "lb")
-data$HtM <- ConvertHt(data, ht = "HtIN", ht_unit = "in")
+data$WtKG <- ConvertWt(data, wt = "WeightLB", wt_unit = "lb")
+data$HtM <- ConvertHt(data, ht = "HeightIN", ht_unit = "in")
 ```
